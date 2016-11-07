643eab7eae6cf6e8a55b01c02f311cdef00bcedf
Nat <bnat1@umbc.edu>
2016-11-07 12:57:59 -0500

    complete

--- CSP.py
+++ CSP.py
@@ -2,26 +2,32 @@ import sys
 
 class Sudoku:
 	grid = []
-	numRemainingSquares = 81
+	numRemainingSquares = 0
 
 	def __init__(self, filename):
 		with open(filename, 'r') as f:			
 			for line in f:
 				row = []
 				for c in line:
-					tempdict = {}
-					tempdict['text'] = c
-					tempdict['domain'] = []
-					row.append(tempdict)
+					if c !='\n':
+						tempdict = {}
+						tempdict['text'] = c
+						tempdict['domain'] = []
+						row.append(tempdict)
+						if c == '-':
+							self.numRemainingSquares += 1
 				self.grid.append(row)
-				
-	def printGrid(self):
-		for i in range(len(self.grid)):
-			line = ""
-			for j in range(len(self.grid)):
-				line += self.grid[i][j]['text']
-			print(line)
+	def isComplete(self):
+		return self.numRemainingSquares == 0
 
+	def printGrid(self):
+		printstr = ""
+		for i in range(len(self.grid)):
+			for j in range(len(self.grid)):
+				printstr += self.grid[i][j]['text']
+			if i != len(self.grid) - 1:
+				printstr += '\n'
+		print(printstr)
 	def checkVertical(self, row, col, val):
 		isValid = True
 		#start at top, look down
@@ -50,14 +56,18 @@ class Sudoku:
 					isValid = False
 		return isValid
 
+	def debug(self):
+		for i in range(len(self.grid)):
+			print(self.grid[i])
+
 	def updateDomain(self):
 		#iterate through grid
 		for i in range(len(self.grid)):
 			for j in range(len(self.grid)):
-				#check empty squares
+				#reset domain
+				del self.grid[i][j]['domain'][:]	
 				if self.grid[i][j]['text'] == '-':
 					#empty domain list
-					del self.grid[i][j]['domain'][:]
 					#test domain values, add to domain list
 					for testDomainVal in range(1,10):
 						testDomainVal = str(testDomainVal)
@@ -65,13 +75,42 @@ class Sudoku:
 						self.checkVertical(i,j,testDomainVal) and \
 						self.checkGroup(i,j,testDomainVal):
 							self.grid[i][j]['domain'].append(testDomainVal)
+	def selectUnassignedVar(self):
+		#select smallest nonempty domain
+		#if none exists, return error
+		minDomainLen = float('inf')
+		minDomainRow = 0
+		minDomainCol = 0
+		for i in range(len(self.grid)):
+			for j in range(len(self.grid)):
+				testDomainLen = len(self.grid[i][j]['domain']) 
+				#is tie an issue?
+				if testDomainLen != 0 and testDomainLen < minDomainLen:
+					minDomain = testDomainLen
+					minDomainRow = i
+					minDomainCol = j
+		return minDomainRow, minDomainCol
 
 	def recursiveBacktrackSolve(self):
-		print("recurssiveBacktrackSolve skeleton call")
-	
-	def getNextVariable(self):
-		print("getNextVariable skeleton call")
-
+		if self.isComplete():
+			return True
+		row, col = self.selectUnassignedVar()
+		domain = list(self.grid[row][col]['domain'])
+		for val in domain:
+			# print("adding %s to %d %d" % (val, row, col))
+			self.grid[row][col]['text'] = val
+			self.updateDomain()
+			self.numRemainingSquares -= 1
+			result = self.recursiveBacktrackSolve()
+			if result:
+				return True
+			#remove value after reaching a dead end from this assignment
+			# print("removing %s to %d %d" % (val, row, col))
+			self.grid[row][col]['text'] = '-'
+			self.updateDomain()
+			self.numRemainingSquares += 1
+		#dead end
+		return False
 
 def main(argc, argv):
 	filename = argv[1]
@@ -79,13 +118,13 @@ def main(argc, argv):
 	if argc != 2:
 		sys.exit()
 	sudoku = Sudoku(filename)
+	# sudoku.printGrid()
 	sudoku.updateDomain()
-	sudoku.printGrid()
-	print(sudoku.grid[1][7]['domain'])
-
-
-	#sudoku = recursiveBacktrackSolve()
-	#sudoku.printGrid()
+	result = sudoku.recursiveBacktrackSolve()
+	if result:
+		sudoku.printGrid()
+	else:
+		print("unsolvable puzzle")
 
 if __name__ == '__main__':
 	main(len(sys.argv), sys.argv)
